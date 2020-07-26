bd4e8ed9ba104a37741e71e4d5f4380b823463c5
Majed <majed@mbpalkhasbmajed.lan>
2020-07-26 19:43:15 +0300

    first commit

---
+++ Village/Assets/scripts/CameraConteroll.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraConteroll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 mouseOrigin = new Vector3();
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        onMouseDown();
+    }
+    
+    void onMouseDown(){
+        if(Input.GetMouseButtonDown(0)){
+            mouseOrigin = Input.mousePosition;
+        }
+        if(Input.GetMouseButton(0))
+        {
+            // Debug.Log("down");
+            CameraController();
+        }
+        
+
+    }
+    
+    void CameraController()
+    {
+        // Debug.Log(Input.mousePosition);
+        Camera camera = Camera.main;
+        Vector3 mouseDif = mouseOrigin - Input.mousePosition;
+        Debug.Log(mouseDif/108);
+        camera.transform.position = camera.transform.position + mouseDif/108;
+        mouseOrigin = Input.mousePosition;
+
+    }
+    
+}
+
+
